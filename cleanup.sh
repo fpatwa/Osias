f4101b4121cc762daad95047bd51ab28a2428f94
James Benson <james.o.benson@gmail.com>
2020-09-16 19:37:27 +0000

    Update deploy_ceph.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,53 +1,66 @@
 #!/bin/bash
 
-NODES="$@"
+function_name=$1
 
+function cleanup_master() {
+    # Destroy openstack and delete images
+    cd /opt/kolla
+    source venv/bin/activate
+    kolla-ansible -i multinode destroy --yes-i-really-really-mean-it --include-images
+    deactivate
+    sudo pip3 uninstall -qy python-openstackclient
 
-# Destroy openstack and delete images
-cd /opt/kolla
-source venv/bin/activate
-kolla-ansible -i multinode destroy --yes-i-really-really-mean-it --include-images
-deactivate
-pip3 uninstall -qy python-openstackclient
+    # Cleanup refstack
+    cd ~
+    sudo rm -fr refstack-client
 
-# Cleanup refstack
-cd ~
-sudo rm -r refstack-client
+    # Remove ansible configs
+    sudo rm -f /etc/ansible/ansible.cfg
 
+    # Get all the fsids
+    fsids=$(sudo cephadm ls |grep fsid |cut -d"\"" -f 4|uniq)
 
-# Remove ansible configs
-sudo rm -f /etc/ansible/ansible.cfg
+    # Remove ceph cluster & destroy ceph partition
+    for id in $fsids;do sudo ./cephadm rm-cluster --force --fsid $id;done
+    sudo rm -fr /etc/ceph
+}
 
-# Remove ceph cluster & destroy ceph partition
-sudo cephadm shell -- ceph cluster fsid |& tee /tmp/cephinfo
+function cleanup_nodes() {
+    # ceph services cleanup
+    ceph_services=$(sudo systemctl |grep ceph |grep "\.service" |awk '{print $2}')
+    for ceph_service in $ceph_services
+    do
+        systemctl stop $ceph_service
+	    sleep 1
+        systemctl disable $ceph_service
+    done
 
-# Grabs the fsid from the output of above
-grep -oP "fsid\s+\K\w+\W\w+\W\w+\W\w+\W\w+" /tmp/cephinfo > /tmp/fsid
-sudo ./cephadm rm-cluster --force --fsid "$(cat /tmp/fsid)"
-sudo rm -r /etc/ceph
+    sudo rm -fr /etc/systemd/system/ceph*
+    sudo rm -fr /etc/systemd/system/*/ceph*
+    sudo rm -fr /lib/systemd/system/ceph*
 
-for node in $NODES
-do
-    echo ""
-    echo "INFO: Working on Node [$node]"
+    sudo systemctl daemon-reload
+    sudo systemctl reset-failed
 
-    # This will remove: all stopped containers, all networks not used by at least one container, 
+    # This will remove: all stopped containers, all networks not used by at least one container,
     # all volumes not used by at least one container, all dangling images, all build cache
-    ssh $node sudo docker stop $(docker ps -aq)
-    ssh $node sudo docker rm $(docker ps -aq)
-    ssh $node sudo docker system prune --volumes --force -a
-    ssh $node sudo rm -r /etc/kolla
+    sudo docker rm -f $(docker ps -aq)
+    sudo docker system prune --volumes --force -a
+    sudo rm -fr /etc/kolla
 
     # Get the last device in the device list (most likely this is the secondary disk used for ceph)
-    device=$(ssh $node lsblk |grep disk |tail -1 |awk '{print $1}')
+    device=$(sudo lsblk |grep disk |tail -1 |awk '{print $1}')
     echo "INFO: Working working on device [$device]"
 
-    ssh $node sudo umount /dev/$device
-    ssh $node sudo sgdisk --zap-all -- /dev/$device
-    ssh $node sudo partprobe || true
-    ssh $node sudo wipefs -af /dev/$device
-done
+    sudo umount /dev/$device
+    sudo sgdisk --zap-all -- /dev/$device
+    sudo partprobe || true
+    sudo wipefs -af /dev/$device
+    # remove all logical devices that use the /dev/mapper driver
+    sudo dmsetup remove_all
 
-echo ""
-echo "INFO: Cleanup of nodes complete"
-echo ""
+    # Cleanup all files/dirs in the ubuntu home directory
+    #rm -fr /home/ubuntu/*
+}
+
+$function_name
--- deploy_openstack.sh
+++ deploy_openstack.sh
@@ -2,7 +2,5 @@
 
 cd /opt/kolla
 source venv/bin/activate
-kolla-ansible -i multinode prechecks
-kolla-ansible -i multinode pull
 kolla-ansible -i multinode deploy
 kolla-ansible -i multinode post-deploy
