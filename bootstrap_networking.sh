4fb5a208a90c7449d677fe525b42482f1efda207
James Benson <james.o.benson@gmail.com>
2020-10-22 19:00:48 +0000

    Fix legacy bridge commands

--- bootstrap_networking.sh
+++ bootstrap_networking.sh
@@ -2,18 +2,17 @@
 
 set -euxo pipefail
 
-sudo apt-get install bridge-utils -qqy
-
-sudo sh -c 'cat > /etc/rc.local <<__EOF__
+sudo sh -c "cat > /etc/rc.local <<__EOF__
 #!/bin/sh -e
 
-ip link add veno0 type veth peer name veno1
-ifconfig veno0 up
-ifconfig veno1 up
-brctl addif br0 veno0
+ip a | grep -Eq ': veno1.*state UP' || sudo ip link add veno0 type veth peer name veno1
+ip link set veno0 up
+ip link set veno1 up
+ip link set veno0 master br0
 exit 0
-__EOF__'
+__EOF__"
 
 sudo chmod +x /etc/rc.local
 sudo chmod 755 /etc/rc.local
 sudo chown root:root /etc/rc.local
+sudo /etc/rc.local
