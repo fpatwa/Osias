49728c0dae7846ef9eb7cc7709469726eb932602
James Benson <james.o.benson@gmail.com>
2020-10-24 01:48:34 +0000

    Update bootstrap_networking.sh, deploy_openstack.sh files

--- bootstrap_networking.sh
+++ bootstrap_networking.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install python3-dev libffi-dev gcc libssl-dev python3-pip python
 # For Post-deploy openstack:
 sudo pip3 install python-openstackclient
 
-sudo sh -c 'cat > /etc/rc.local <<__EOF__
+sudo sh -c "cat > /etc/rc.local <<__EOF__
 #!/bin/sh -e
 
 ip a | grep -Eq ': veno1.*state UP' || sudo ip link add veno0 type veth peer name veno1
@@ -18,7 +18,7 @@ ip link set veno0 up
 ip link set veno1 up
 ip link set veno0 master br0
 exit 0
-__EOF__'
+__EOF__"
 
 sudo chmod +x /etc/rc.local
 sudo chmod 755 /etc/rc.local
