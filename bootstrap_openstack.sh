53765eb29f2539eca6e7ce192b81e3fc0bc14fd1
James Benson <james.o.benson@gmail.com>
2020-10-25 17:22:44 +0000

    Repair ceph bugs.

--- bootstrap_openstack.sh
+++ bootstrap_openstack.sh
@@ -7,6 +7,8 @@ source venv/bin/activate
 
 kolla-genpwd
 kolla-ansible -i multinode certificates
+
+# This bootstrap is necessary to prep for ceph and openstack deployment.
 kolla-ansible -i multinode bootstrap-servers
 
 # Enable docker group for $USER
--- deploy_openstack.sh
+++ deploy_openstack.sh
@@ -5,5 +5,7 @@ set -euxo pipefail
 cd /opt/kolla
 source venv/bin/activate
 
+# This bootstrap is needed to repair some ceph bugs introduced after ceph deployment
+kolla-ansible -i multinode bootstrap-servers
 kolla-ansible -i multinode deploy
 kolla-ansible -i multinode post-deploy
