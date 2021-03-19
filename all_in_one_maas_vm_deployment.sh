cf063007d8b14674e05cf8318bc9c60099d203ee
James Benson <james.o.benson@gmail.com>
2021-03-19 19:43:43 +0000

    Update all_in_one_maas_vm_deployment.sh

--- all_in_one_maas_vm_deployment.sh
+++ all_in_one_maas_vm_deployment.sh
@@ -50,13 +50,9 @@ configure_virsh () {
     sudo virsh net-create virsh.default.net.xml
     grep -v '^#' /etc/netplan/50-cloud-init.yaml > /tmp/50-cloud-init.yaml
     grep -v '^#' /etc/netplan/50-cloud-init.yaml >> /tmp/50-cloud-init.yaml
-    cat /tmp/50-cloud-init.yaml
     sed -i '1!{/^network/d;}' /tmp/50-cloud-init.yaml
-    cat /tmp/50-cloud-init.yaml
     sed -i '0,/ethernets/{s/ethernets/bridges/}' /tmp/50-cloud-init.yaml
-    cat /tmp/50-cloud-init.yaml
     sed -i '0,/ens4/{s/ens4/br-eth0/}' /tmp/50-cloud-init.yaml
-    cat /tmp/50-cloud-init.yaml
     sed -i '/br-eth0/a             interfaces:\n            - ens4' /tmp/50-cloud-init.yaml
     cat /tmp/50-cloud-init.yaml
     sudo cp /tmp/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml
