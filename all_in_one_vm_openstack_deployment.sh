aa45bc9bcf79f3e50c29007f6668b341a0505434
Farhan Patwa <farhan.patwa@gmail.com>
2021-05-24 15:59:47 +0000

    Update all_in_one_vm_openstack_deployment.sh

--- all_in_one_vm_openstack_deployment.sh
+++ all_in_one_vm_openstack_deployment.sh
@@ -62,5 +62,5 @@ export VM_PROFILE="{\"Data_CIDR\": \"10.100.0.0/16\", \"DNS_IP\": \"$my_dns\"}"
 export VM_DEPLOYMENT_CIDR="${my_ip}/32"
 python3 -c "import json;import os;vm_profile=json.loads(os.getenv('VM_PROFILE'));vm_profile['vm_deployment_cidr']=os.getenv('VM_DEPLOYMENT_CIDR');vm_profile_file = open('vm_profile', 'w');vm_profile_file.write(json.dumps(vm_profile));vm_profile_file.close()"
 #
-export VM_PROFILE=$(cat test/vm_profile)
+export VM_PROFILE=$(cat vm_profile)
 python3 -u deploy.py create_travisci_multinode --VM_PROFILE "$VM_PROFILE"
