37a1556331481f3924c7fe51d274f10b6552ecd8
Farhan Patwa <farhan.patwa@gmail.com>
2021-05-24 15:56:23 +0000

    Update all_in_one_vm_openstack_deployment.sh

--- all_in_one_vm_openstack_deployment.sh
+++ all_in_one_vm_openstack_deployment.sh
@@ -58,8 +58,8 @@ my_ip=$(get_vm_profile)
 #
 my_dns=$(systemd-resolve --status |grep "DNS Servers"|awk '{print $3}')
 #
-VM_PROFILE="{\"Data_CIDR\": \"10.100.0.0/16\", \"DNS_IP\": \"$my_dns\"}"
-VM_DEPLOYMENT_CIDR="${my_ip}/32"
+export VM_PROFILE="{\"Data_CIDR\": \"10.100.0.0/16\", \"DNS_IP\": \"$my_dns\"}"
+export VM_DEPLOYMENT_CIDR="${my_ip}/32"
 python3 -c "import json;import os;vm_profile=json.loads(os.getenv('VM_PROFILE'));vm_profile['vm_deployment_cidr']=os.getenv('VM_DEPLOYMENT_CIDR');vm_profile_file = open('vm_profile', 'w');vm_profile_file.write(json.dumps(vm_profile));vm_profile_file.close()"
 #
 export VM_PROFILE=$(cat test/vm_profile)
