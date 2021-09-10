7cc241addd536c358de97825c2e01648250299a5
Farhan Patwa <farhan.patwa@gmail.com>
2021-09-10 20:33:36 +0000

    Update bootstrap_networking.sh

--- bootstrap_networking.sh
+++ bootstrap_networking.sh
@@ -5,8 +5,17 @@ set -euxo pipefail
 # Add br0 bridge if it does not exist
 br0_exists(){ ip addr show br0 &>/dev/null; }
 if ! br0_exists; then
-  sudo brctl addbr br0
-  sudo brctl addif br0 eth0
+  public_interface=$(route -n | awk '$1 == "0.0.0.0" {print $8}')
+  MY_IP=$(ip -o -4 addr list "${public_interface}" | awk '{print $4}')
+  MY_GATEWAY=$(ip route show 0.0.0.0/0 dev eth0 | cut -d\  -f3)
+  #
+  ip link add br0 type bridge
+  ip link set eth0 master br0
+  ip link set br0 up
+  ip addr add $MY_IP dev br0
+  #
+  route add -net 0.0.0.0 gw $MY_GATEWAY dev br0
+  ip route del default via $MY_GATEWAY dev eth0
   ip a
 fi
 
