06be920033bc38bf0e6ee8af6079e1b9d691fab3
James Benson <james.o.benson@gmail.com>
2021-09-30 13:04:42 +0000

    debugging 7 failures: adding another bootstrap.

--- deploy_openstack.sh
+++ deploy_openstack.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 cd /opt/kolla
 source venv/bin/activate
 
+kolla-ansible -i multinode bootstrap-servers
 kolla-ansible -i multinode deploy
 kolla-ansible -i multinode post-deploy
 
