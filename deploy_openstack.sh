98a239295dc77de194fc4ac52599917fa5630934
James Benson <james.o.benson@gmail.com>
2021-10-01 12:47:14 +0000

    Update deploy_openstack.sh

--- deploy_openstack.sh
+++ deploy_openstack.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 cd /opt/kolla
 source venv/bin/activate
-kolla-ansible -i multinode bootstrap-servers
+#kolla-ansible -i multinode bootstrap-servers
 kolla-ansible -i multinode deploy
 kolla-ansible -i multinode post-deploy
 deactivate nondestructive
